18ff7236d62aabc44e9e0ff6b59ab2db36ddb54d
Jukka Suomela <jukka.suomela@iki.fi>
2012-09-20 20:44:32 +0300

    A new function: convert_file.

--- lib/FileTools.py
+++ lib/FileTools.py
@@ -4,7 +4,7 @@ import sys
 def parse_tsv(l):
     return [ s if s != '' else None for s in l.rstrip('\r\n').split('\t') ]
 
-def import_file(conn, filename, encoding, fields, header, insert, hook=None):
+def process_file(store, filename, encoding, fields, header, hook=None):
     relevant, total = 0, 0
     with codecs.open(filename, 'rU', encoding) as f:
         check_header = header is not None
@@ -21,9 +21,22 @@ def import_file(conn, filename, encoding, fields, header, insert, hook=None):
                 if r is None:
                     continue
                 relevant += 1
-                try:
-                    conn.execute(insert, r)
-                except:
-                    sys.stderr.write("%s\n%s\n" % (insert, str(r)))
-                    raise
-    print "%-55s %8d/%-8d" % (filename, relevant, total)
+                store(r)
+    print "%-60s %7d/%d" % (filename, relevant, total)
+
+
+def import_file(conn, filename, encoding, fields, header, insert, hook=None):
+    def store(r):
+        try:
+            conn.execute(insert, r)
+        except:
+            sys.stderr.write("%s\n%s\n" % (insert, str(r)))
+            raise
+    process_file(store, filename, encoding, fields, header, hook)
+
+def convert_file(fin, fout, encoding, fields, header, hook=None):
+    with open(fout, 'w') as f:
+        def store(r):
+            f.write('\t'.join(r))
+            f.write('\n')
+        process_file(store, fin, encoding, fields, header, hook)
