30e78f8fe43606ce4abfa682d073cf8f28e5a28b
Jukka Suomela <jukka.suomela@iki.fi>
2012-11-18 15:18:13 +0200

    Version number.

--- src/TypesVersion.py
+++ src/TypesVersion.py
@@ -1,4 +1,4 @@
 DESCRIPTION = "Type and hapax accumulation curves"
 AUTHOR = "Jukka Suomela"
-VERSION = "2012-11-03"
+VERSION = "2012-11-18"
 YEAR = "2012"
--- src/version.c
+++ src/version.c
@@ -2,5 +2,5 @@
 
 const char * const DESCRIPTION = "Type and hapax accumulation curves";
 const char * const AUTHOR = "Jukka Suomela";
-const char * const VERSION = "2012-11-03";
+const char * const VERSION = "2012-11-18";
 const char * const YEAR = "2012";
