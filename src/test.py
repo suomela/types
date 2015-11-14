34fa0e299632e90140c3f6e91f13dc5e226db61f
Jukka Suomela <jukka.suomela@iki.fi>
2015-11-15 00:54:16 +0200

    Check that all relevant modules can be loaded.

--- src/test.py
+++ src/test.py
@@ -1,9 +1,14 @@
-import sys
-import codecs
 import cPickle
+import codecs
+import collections
+import json
 import multiprocessing
 import optparse
+import os
+import os.path
 import sqlite3
+import subprocess
+import sys
 
 assert sys.version_info >= (2,6)
 print "OK"
